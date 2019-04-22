Units corrected

mpsmechanics/dothemaths/angular.py
@@ -53,12 +53,12 @@ def calc_direction_vectors(disp, plt_pr, movement_filter):
 
     dir_v = np.array([1, slope])
 
-    e_alpha = np.linalg.norm(dir_v)*dir_v
+    e_alpha = 1./np.linalg.norm(dir_v)*dir_v
     e_beta  = np.array([-e_alpha[1], e_alpha[0]])
  
     if(plt_pr["visual check"]):
         _plot_data_vectors(xs, ys, X, Y, e_alpha, e_beta, plt_pr)
-    
+
     return e_alpha, e_beta
 
 

mpsmechanics/dothemaths/heartbeat.py
@@ -7,6 +7,7 @@ Aashild Telle / Simula Research Labratory / 2018-2019
 
 """
 
+import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -104,7 +105,7 @@ def _get_beat_maxima(disp_norm, local_intervals):
     return maxima
 
 
-def calc_beat_maxima_time(data, scale, T_max, \
+def calc_beat_maxima_time(data, scale, \
         plt_pr = {"visual check" : False}):
     """
 
@@ -113,7 +114,6 @@ def calc_beat_maxima_time(data, scale, T_max, \
 
     Arguments:
         data - numpy array, displacement values over time
-        T_max     - last time value
         plt_pr - dictionary defining visual output
 
     Returns:
@@ -126,14 +126,13 @@ def calc_beat_maxima_time(data, scale, T_max, \
     local_intervals = _get_local_intervals(data, eps)
     maxima = _get_beat_maxima(data, local_intervals)
     
-    #if(plt_pr["visual check"]):
-    #    idt = plt_pr["idt"]
-    #    _plot_disp_thresholds(data, scale, maxima, eps, idt, T_max)
+    if(plt_pr["visual check"]):
+        _plot_disp_thresholds(data, scale, maxima, eps, plt_pr)
 
     return maxima
 
 
-def calc_beat_maxima_2D(data, movement, scale=1, T_max=1, \
+def calc_beat_maxima_2D(data, movement, scale=1, Tmax=1, \
         plt_pr = {"visual check" : False}):
     """
 
@@ -142,7 +141,7 @@ def calc_beat_maxima_2D(data, movement, scale=1, T_max=1, \
 
     Arguments:
         data   - T x X x Y x 2 numpy array, displacement values
-        T_max  - last time value, optional
+        Tmax  - last time value, optional
         plt_pr - dictionary defining visual output, optional
 
     Returns:
@@ -152,10 +151,10 @@ def calc_beat_maxima_2D(data, movement, scale=1, T_max=1, \
 
     disp_norm = op.calc_norm_over_time(data, movement)
     
-    return calc_beat_maxima_time(disp_norm, scale, T_max, plt_pr)
+    return calc_beat_maxima_time(disp_norm, scale, plt_pr)
 
 
-def _plot_disp_thresholds(disp_norm, scale, maxima, eps, idt, T_max):
+def _plot_disp_thresholds(disp_norm, scale, maxima, eps, plt_pr):
     """
 
     Plots values, maxima, mean value and mean value*(1+eps) for a 
@@ -170,12 +169,14 @@ def _plot_disp_thresholds(disp_norm, scale, maxima, eps, idt, T_max):
         scale     - scale to get in SI units
         maxima    - time steps for maxima values
         eps       - buffer value
-        idt       - filename idt
-        T_max     - last time value
+        plt_pr    - gives general values
+
     """
-    
+   
+    path = plt_pr["path"]
+    Tmax = plt_pr["Tmax"]
  
-    t = np.linspace(0, T_max, len(disp_norm))
+    t = np.linspace(0, Tmax, len(disp_norm))
    
     disp_scaled = scale*disp_norm
 
@@ -199,12 +200,8 @@ def _plot_disp_thresholds(disp_norm, scale, maxima, eps, idt, T_max):
 
     plt.xlabel('Time (s)')
 
-    # make plot dir if it doesn't already exist
-    de = io.get_os_delimiter()
-    path = de.join(("Figures" + de + idt).split(de)[:-1])
-    io.make_dir_structure(path)
-
-    plt.savefig("Figures" + de + idt + "_mean.png", dpi=1000)
+    filename = os.path.join(path, "mean.png")
+    plt.savefig(filename, dpi=1000)
 
     plt.clf()
 

mpsmechanics/iofuns/motion_data.py
@@ -15,18 +15,17 @@ import matplotlib.pyplot as plt
 import mps
 
 
-def read_file(filename, xlen):
-
+def read_file(filename):
     if(".nd2" in filename):
-        return _read_file_nd2(filename, xlen)
+        return _read_file_nd2(filename)
     elif(".csv" in filename):
-        return _read_file_csv(filename, xlen)
+        return _read_file_csv(filename)
     else:
         print("Uknown file formate")
         exit(-1)
 
 
-def _read_file_nd2(filename, xlen):
+def _read_file_nd2(filename):
 
     mps_data = mps.MPS(filename)
     motion = mps.MotionTracking(mps_data)
@@ -37,23 +36,18 @@ def _read_file_nd2(filename, xlen):
     # reshape; t as outer dimension - or??
 
     X, Y, D, T = data.shape
-
-    dx = xlen/X
-
     data_disp = np.zeros((T, X, Y, D))
 
     for t in range(T):
         for x in range(X):
             for y in range(Y):
                 for d in range(D):
-                    data_disp[t, x, y, d] = dx*data[x, y, d, t]
+                    data_disp[t, x, y, d] = data[x, y, d, t]
 
-    scale = _get_scale(data_disp)
-
-    return scale*data_disp, 1./scale
+    return data_disp
 
 
-def _read_file_csv(filename, xlen):
+def _read_file_csv(filename):
     """
 
     Reads the input file, where the file is assumed to be a csv file on
@@ -76,19 +70,15 @@ def _read_file_csv(filename, xlen):
 
     Arguments:
         filename - csv file
-        xlen - length of picture, in meters
 
     Returns:
         4-dimensional numpy array, of dimensions T x X x Y x 2
-        scale - scale data points with this to get original magnitude
 
     """
 
     f = open(filename, 'r')
 
     T, X, Y = map(int, str.split(f.readline(), ","))
-    dx = xlen/X
-
     data = np.zeros((T, X, Y, 2))
 
     for t in range(T):
@@ -96,58 +86,10 @@ def _read_file_csv(filename, xlen):
             for j in range(Y):
                 str_values = str.split(f.readline().strip(), ",")
                 d = list(map(float, str_values))
-                data[t, i, j] = dx*np.array(d)
+                data[t, i, j] = np.array(d)
     
     f.close()
 
-    scale = _get_scale(data)
-
     # omit first value; reference configuration
 
-    return scale*data, 1./scale
-
-
-def _get_scale(data):
-    """
-
-    Scales data such that the middle value is on standard form, i.e.
-    in the range [0.1, 1]
-
-    """
-
-
-    # get maximum and minimum; scale middle value
-
-    mmax = abs(np.max(data))
-    mmin = abs(np.min(data))
-
-    mid = 0.5*(mmax + mmin)
-
-    # scale from above or below; dependin on scale
-
-    scale = 1
-
-    while(mid > 1):
-        mid = mid/10
-        scale = scale/10
-
-    while(mid < 1E-1):
-        mid = mid*10
-        scale = scale*10
-
-    return scale
-
-
-if __name__ == "__main__":
-    
-    try:
-        f_in = sys.argv[1]
-    except:
-        print("Give file name as first argument")
-        exit(-1)
-
-    assert(read_file_csv(f_in, 1) is not None)
-    make_dir_structure("Figures")         # no return value
-
-    print("All tests passed for io_funs.py")
-
+    return data

mpsmechanics/metrics/metric_values.py
@@ -19,7 +19,8 @@ from ..dothemaths import heartbeat as hb
 from . import metric as mt
 from . import metric_xy as mt_xy
 
-def calc_metrics(disp_data, ind_list, scale, dt, plt_pr, movement):
+def calc_metrics(disp_data, ind_list, scale, dt, plt_pr, \
+        threshold, movement):
     """
 
     Arguments:
@@ -31,6 +32,7 @@ def calc_metrics(disp_data, ind_list, scale, dt, plt_pr, movement):
         dt - temporal difference
         dx - spacial difference
         plt_pr - dictionary determining visual output
+        threshold - for prevalence
         movement - movement filter; numpy boolean array
 
     Returns:
@@ -56,12 +58,10 @@ def calc_metrics(disp_data, ind_list, scale, dt, plt_pr, movement):
     T, X, Y = disp_data.shape[:3]
     T_max = dt*T
     time = np.linspace(0, T_max, T)
-    threshold = 2*10E-6*dt/scale
 
     # and some useful variables - TODO these might be stored in a "cache"?
     disp_time = op.calc_norm_over_time(disp_data, movement)
-    maxima = hb.calc_beat_maxima_time(disp_time, scale, T_max, \
-            plt_pr)
+    maxima = hb.calc_beat_maxima_time(disp_time, scale, plt_pr)
     e_alpha, e_beta = an.calc_direction_vectors(disp_data, plt_pr, movement)
     
     e_alpha = (e_alpha, "x_projection", "X projection")
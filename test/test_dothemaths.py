Units tests fix

test/test_dothemaths.py
@@ -8,13 +8,14 @@ import numpy as np
 
 import mpsmechanics as mc
 
-def test_calc_projection_vectors(shape):
+def test_calc_projection_vectors():
     """
 
     Unit test for mpsmechanics/dothemaths/angular
         -> calc_projection_vectors
 
     """
+    shape = (5, 4, 3, 1)
 
     alpha = np.pi/2
     data_org = np.tile(np.array((1, 1)), shape)
@@ -24,13 +25,14 @@ def test_calc_projection_vectors(shape):
                        mc.calc_projection_vectors(data_org, alpha))
 
 
-def test_calc_projection_fraction(shape):
+def test_calc_projection_fraction():
     """
 
     Unit test for mpsmechanics/dothemaths/angular
         -> calc_projection_fractions
 
     """
+    shape = (5, 4, 3, 1)
 
     alpha = np.pi/2
     data_org = np.tile(np.array((1, 1)), shape)
@@ -40,7 +42,8 @@ def test_calc_projection_fraction(shape):
                        mc.calc_projection_fraction(data_org, alpha))
 
 
-def test_calc_angle_diff(shape):
+def test_calc_angle_diff():
+    shape = (5, 4, 3, 1)
     alpha = 0
     data_org = np.tile(np.array((1, 1)), shape)
     data_exp = (np.pi/4)*np.ones(shape)
@@ -48,13 +51,14 @@ def test_calc_angle_diff(shape):
     assert np.allclose(data_exp,
                        mc.calc_angle_diff(data_org, alpha))
 
-def test_flip_values(shape):
+def test_flip_values():
     """
 
     Unit test for mpsmechanics/dothemaths/angular
         -> flip_values
 
     """
+    shape = (5, 4, 3, 1)
 
     data_org = np.tile(np.array((1, -1)), shape)
     data_exp = np.tile(np.array((-1, 1)), shape)
@@ -63,18 +67,11 @@ def test_flip_values(shape):
                        mc.flip_values(data_org))
 
 
-def test_angular():
-    """
-
-    All unit tests for mpsmechanics/dothemaths/angular
-
-    """
-    shape = (5, 4, 3, 1)
+if __name__ == "__main__":
+    
+    # angular
+    
     test_calc_projection_vectors(shape)
     test_calc_projection_fraction(shape)
     test_calc_angle_diff(shape)
     test_flip_values(shape)
-
-
-if __name__ == "__main__":
-    test_angular()
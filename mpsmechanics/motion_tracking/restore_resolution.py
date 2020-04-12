bd7c21bb706f1c33dbd2c5386492f45d2328a0c5
mewasul <aashild@simula.no>
2020-04-12 21:47:58 +0200

    fix unit test

--- mpsmechanics/motion_tracking/restore_resolution.py
+++ mpsmechanics/motion_tracking/restore_resolution.py
@@ -12,18 +12,20 @@ from ..mechanical_analysis.filters import filter_constrained
 def apply_filter(motion_data, type_filter, sigma):
     """
 
-    Attempts to restore resolution which was lost due to downsampling to pixel
-    (integer) values.
+    Attempts to restore resolution which was lost due to downsampling
+    to pixel (integer) values.
 
     Args:
         motion_data - T x X x Y x 2 numpy array
         type_filter - "gaussian" or "downsampling"
         sigma - argument to filter method, either
           * sigma in x and y direction for gaussian filter
-          * number of pixel blocks to combine using downsampling approach
+          * number of pixel blocks to combine using downsampling
+              approach
 
     Return:
-        smoothered / downsampled data, similar shape as the original motion_data
+        smoothered / downsampled data, similar shape as the original
+            motion_data
 
     """
 
@@ -36,7 +38,7 @@ def apply_filter(motion_data, type_filter, sigma):
     if type_filter == "gaussian":
         return gaussian_filter(motion_data, [0, sigma, sigma, 0])
 
-    elif type_filter == "gaussian_mask":
+    if type_filter == "gaussian_mask":
         mask = filter_constrained(motion_data, 5)[0]
 
         return gaussian_filter_with_mask(motion_data, sigma, mask)
@@ -72,25 +74,29 @@ def gaussian_filter_with_mask(
         sigma: float,
         mask: np.ndarray) -> np.ndarray:
     """
-    
+
     Args:
-        displacement/motion data – numpy array of dimension T x X x Y x 2
+        displacement/motion data – T x X x Y x 2 numpy array
         sigma - parameter for gaussian filter
-        mask - which values to perform diffusion for; X x Y array
+        mask - which values to perform diffusion for;
+            X x Y numpy array
 
     """
 
     assert len(motion_data.shape) == 4 and motion_data.shape[-1] == 2, \
-        f"Error: Unexpected shape for motion data: {motion_data.shape}; expected T x X x Y x 2"
-    
+        f"Error: Unexpected shape for motion data: {motion_data.shape};" + \
+            " expected T x X x Y x 2"
+
     assert mask.shape == motion_data.shape[1:3], \
-        f"Error: Unexpected shape for mask: {mask.shape}; expected {motion_data.shape[1:3]}"
+        f"Error: Unexpected shape for mask: {mask.shape};" + \
+            f" expected {motion_data.shape[1:3]}"
 
     filtered_data = np.zeros_like(motion_data)
 
     for t in range(len(motion_data)):
         for i in range(2):
             data_loc = gaussian_filter(motion_data[t, :, :, i], sigma)
+            data_loc[np.logical_not(mask)] = 0
 
             filtered_data[t, :, :, i] = \
                     np.where(mask, motion_data[t, :, :, i], data_loc)
